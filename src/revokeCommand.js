30a8c60ac91a871f4e20c6fddd3346bf3f532148
Arnab <arnabsarkar2000@gmail.com>
2023-11-29 01:54:20 +0530

    migrate asset fetch code from js to python
     due to compatibility issue replaced the code type for 1 file

--- src/revokeCommand.js
+++ src/revokeCommand.js
@@ -4,9 +4,16 @@ const {protobotAuthToken, protobotClientID} = process.env;
 
 const rest = new REST({ version: '10' }).setToken(protobotAuthToken);
 
-const tokenID = 'tokenID';
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-// for global commands
-rest.delete(Routes.applicationCommand(protobotClientID, tokenID))
-	.then(() => console.log('Successfully deleted application command with token ID:',tokenID))
-	.catch(console.error);
+rl.question('Enter token to delete for the command: ', (tokenID) => {
+  // for global commands
+  rest.delete(Routes.applicationCommand(protobotClientID, tokenID))
+    .then(() => console.log('Successfully deleted application command with token ID:',tokenID))
+    .catch(console.error);
+  rl.close();
+});
